60827db9cbea166fd61234fd705d42a0af87e291
DrC0okie <timothee.vanhove@heig-vd.ch>
2023-06-09 12:18:04 +0200

    Added device 2 with the leds

--- App/lambda/write.js
+++ App/lambda/write.js
@@ -5,20 +5,13 @@ exports.handler = async (event) => {
     if (event.httpMethod === 'OPTIONS') {
         return {
             statusCode: 200,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Allow-Methods': 'POST',
-                'Access-Control-Allow-Headers': 'Content-Type',
-            },
             body: JSON.stringify('OK'),
         };
     }
-
+    let body = JSON.parse(event.body);
     const params = {
-        topic: 'esp8266/sub',
-        payload: JSON.stringify(event),
+        topic: body.topic,
+        payload: JSON.stringify({ message: body.message }),
         qos: 0
     };
 
@@ -26,23 +19,13 @@ exports.handler = async (event) => {
         await iotdata.publish(params).promise();
         const response = {
             statusCode: 200,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
-            body: JSON.stringify('Message sent'),
+            body: JSON.stringify('Message sent:' + body.message),
         };
         return response;
     } catch (err) {
         console.log(err);
         const response = {
             statusCode: 500,
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
             body: JSON.stringify('An error occurred'),
         };
         return response;
